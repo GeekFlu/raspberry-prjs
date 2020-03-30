0229555828e132bd6661b73b77c470955a162de5
Spike Storm <software.j2ee@gmail.com>
2020-03-29 18:51:29 -0700

    Finally pins correctly wired

--- programas/robot/testing_movement.py
+++ programas/robot/testing_movement.py
@@ -10,35 +10,41 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-d", "--direction", help = "Rovr Direction") 
 parser.add_argument("-s", "--steps",type=int, help = "Number of steps the Rovr should walk")
 parser.add_argument("-r", "--reverse",type=bool, help = "Reverse modo activated")
+parser.add_argument("-t", "--time",type=float, help = "Time executing the command each step")
 # Read arguments from command line 
 args = parser.parse_args() 
   
 if args.direction and args.steps: 
     print("Diplaying Rovr direction as: % s" % args.direction)
     print("Displaying Rovr steps to perform %s" % args.steps)
-    robot = gpiozero.Robot(left=(17,18), right=(27,22))
+    robot = gpiozero.Robot(left=(4,14), right=(17,18))
     if args.reverse:
         print("Activating REVERSE MOdo!!!!!!")
         robot.reverse()
     d = args.direction.lower()
+    if args.time:
+        tt = args.time
+    else:
+        tt = 2
     for i in range(args.steps):
+        
         if d == 'left':
             print("Moving LEFT")
             robot.left()
-            time.sleep(1)
+            time.sleep(tt)
         elif d == 'right':
             print("Moving RIGTH")
             robot.right()
-            time.sleep(1)
+            time.sleep(tt)
         elif d == 'forward':
             print("Moving FORWARD")
             robot.forward()
-            time.sleep(1)
+            time.sleep(tt)
         elif d == 'backward':
             print("Moving BACKWARD")
             robot.backward()
-            time.sleep(1)
-
+            time.sleep(tt)
+        
     robot.stop()
     print("Stoping Rovr")
 
